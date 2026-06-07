b95bf3aedc11804341f5fb8f810f74fc52d4f597
Marius <mariussangwa@gmail.com>
2026-06-07 02:08:22 +0200

    commented the fetch.psm1 module to add more explanations

--- Basics/PwshModules/fetch.psm1
+++ Basics/PwshModules/fetch.psm1
@@ -1,3 +1,11 @@
+<#
+This is a simple sample module Use "Import-Module pathToThisFile" 
+to use this file in the current session. You import this file by 
+only providing the full path to this file. But there is  a way to 
+create a module folder in one of the module directories ($env:PSModulePath) 
+same name as this file and save in this file as a psm1 file and to import
+this module , you will be using the foldername instead of the whole path
+#>
 function Invoke-FetchUsers{
     Param($csvPath)
     return Import-Csv $csvPath
@@ -10,4 +18,13 @@ function Invoke-FormattedUser{
     }
 }
 
-Export-ModuleMember -Function Invoke-FetchUsers
+Export-ModuleMember -Function Invoke-FetchUsers # This line specifies the functions to be imported
+
+
+<#
+Module components
+    -Code files
+    -Additional assemblies
+    -Manifest file
+    -Directory
+#>
